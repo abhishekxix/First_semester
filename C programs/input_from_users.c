1d21bb803eab19a9ce5878c6401ffe9ca370d994
abhisingh127 <48520239+abhisingh127@users.noreply.github.com>
2019-08-21 04:16:19 +0530

    Add files via upload

--- C programs/input_from_users.c
+++ C programs/input_from_users.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 int main()
-{
+{  printf("Enter a character :: ");
     char b = getchar(); //This getchar()helps to take input as a single character
-    printf("You entered %c",b);
+    printf("\nYou entered %c",b);
     char a[20];
-    printf("Enter your name");
+    printf("\nEnter your name :: ");
     scanf("%s",a); //Taking a string as input from the user
     printf("\nHi %s", a);
     int number;
     printf("\nEnter a number :: ");
-    scanf("%d",number);
+    scanf("%d",&number);
     printf("You entered %d",number);
     return 0;
 }
